18e8f544afdab91f65a842f004a80883fd824772
kusumavishu <vishukusuma2003@gmail.com>
2026-09-03 07:12:53 +0530

    Initial project setup

---
+++ src/main/java/com/aits/hivelink_demo/HivelinkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aits.hivelink_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HivelinkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HivelinkDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aits/hivelink_demo/HivelinkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aits.hivelink_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HivelinkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
